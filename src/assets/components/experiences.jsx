f842a96d83732a5583843dd5792fb2c0b435373e
Deepak Singh <explorethe.unknowing536@passinbox.com>
2026-03-10 13:38:46 +0530

    Update experiences.jsx

--- src/assets/components/experiences.jsx
+++ src/assets/components/experiences.jsx
@@ -6,30 +6,32 @@ export default function Experience(){
             </div>
             
             <div className="experience-container center">
-                <div className="experience bottom-animation">
-                    <div className="experience-heading">
-                        <h3 className="experience-title">Frontend Developer Intern, Unified Mentors</h3>
-                        <p className="experience-duration">March 2025- September 2025</p>
+                    <div className="experience bottom-animation">
+                        <div className="experience-heading">
+                            <h3 className="experience-title">Frontend Developer Intern, Unified Mentors</h3>
+                            <p className="experience-duration">March 2025- September 2025</p>
+                        </div>
+                        <div className="experience-description">
+                            <ul>
+                                <li>Built responsive web pages and applied problem-solving skills to reduce load time by 20% and reduce defects by 30%.</li>
+                                <li>Applied industry-standard practices in unit testing, achieved 95% cross-browser compatibility.</li>
+                                <li>Successfully delivered all mandatory projects and side projects within timeframe.</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="experience-description">
-                        <ul>
-                            <li>Built responsive web pages and applied problem-solving skills to reduce load time by 20% and reported defects by 30%</li>
-                            <li>Applied industry-standard practices in unit testing, achieved 95% cross-browsers compatibility.</li>
-                            <li>Successfully delivered all mandatory projects and side projects within period.</li>
-                        </ul>
-                    </div>
-
-                <div className="experience bottom-animation">
-                    <div className="experience-heading">
-                        <h3 className="experience-title">Freelancer</h3>
-                        <p className="experience-duration">September 2025- </p>
-                    </div>
-                    <div className="experience-description">
-                        <ul>
-                            <li>I build clean, modern, and fully responsive websites using HTML, CSS, JavaScript, and React, ensuring they work smoothly across all devices.</li>
-                            <li>Focused on writing optimized and maintainable code to create fast-loading and user-friendly web interfaces.</li>
-                            <li>Available for small tasks like landing pages, UI fixes, HTML/CSS conversions, and React component development with quick delivery.</li>
-                        </ul>
+    
+                    <div className="experience bottom-animation">
+                        <div className="experience-heading">
+                            <h3 className="experience-title">Freelancer</h3>
+                            <p className="experience-duration">September 2025- Present</p>
+                        </div>
+                        <div className="experience-description">
+                            <ul>
+                                <li>I build clean, modern, and fully responsive websites using HTML, CSS, JavaScript, and React, ensuring they work smoothly across all devices.</li>
+                                <li>Focus on writing optimized and maintainable code to create fast-loading and user-friendly web interfaces.</li>
+                                <li>Available for small tasks like landing pages, UI fixes, HTML/CSS conversions, and React component development with quick delivery.</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
