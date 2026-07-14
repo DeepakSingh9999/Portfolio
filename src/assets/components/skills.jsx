3c28056895b923163ec448734ceb462f6dc240dd
Deepak Singh <explorethe.unknowing536@passinbox.com>
2026-07-14 15:00:44 +0530

    Update skills.jsx

--- src/assets/components/skills.jsx
+++ src/assets/components/skills.jsx
@@ -3,88 +3,93 @@ export default function Skills(){
     const skills = [
 
         {id: 1,
+            title: 'JavaScript',
+            classes : 'fa-brands fa-js skill',
+        },
+              
+        {id: 2,
+            title: 'React.js',
+            classes : 'fa-brands fa-react skill',
+        },
+
+        {id: 3,
+            title: 'Node.js',
+            classes : 'fa-brands fa-node-js skill',
+        },
+
+        {id: 4,
+            title: 'SQL',
+            classes : 'fa-solid fa-database skill',
+        },
+
+        {id: 5,
+            title: 'Git',
+            classes : 'fa-brands fa-git-alt skill',
+        },
+               
+        {id: 6,
+            title: 'Gitlab',
+            classes : 'fa-brands fa-gitlab skill',
+        },
+        
+        {id: 7,
             title: 'Python',
             classes : 'fa-brands fa-python skill',
         },
         
-        {id: 2,
-            title: 'JavaScript',
-            classes : 'fa-brands fa-js skill',
-        },
-        
-        {id: 3,
-            title: 'React',
-            classes : 'fa-brands fa-react skill',
-        },
-        
-        {id: 4,
-            title: 'Git',
-            classes : 'fa-brands fa-git-alt skill',
-        },
-        
-        {id: 5,
-            title: 'Github',
-            classes : 'fa-brands fa-github skill',
-        },
-        
-        {id: 6,
+        {id: 8,
             title: 'HTML',
             classes : 'fa-brands fa-html5 skill',
         },
         
-        {id: 7,
+        {id: 9,
             title: 'CSS',
             classes : 'fa-brands fa-css3-alt skill',
         },
         
-        {id: 8,
+        {id: 10,
             title: 'Bootstrap',
             classes : 'fa-brands fa-bootstrap skill',
         },
         
-        {id: 9,
+        {id: 11,
             title: 'Data Structures',
             classes : 'fa-brands fa-buffer skill',
         },
         
-        {id: 10,
+        {id: 12,
             title: 'Algorithms',
             classes : 'fa-solid fa-chart-simple skill',
         },
         
-        {id: 11,
-            title: 'GenAI',
-            classes : 'fa-brands fa-bilibili skill',
-        },
-        
-        {id: 12,
-            title: 'Problem Solving',
-            classes : 'fa-solid fa-lightbulb skill',
-        },
-        
         {id: 13,
-            title: 'Google Workspace',
-            classes : 'fa-solid fa-briefcase skill',
-        },
-        
-        {id: 14,
-            title: 'Communication',
-            classes : 'fa-solid fa-phone skill',
-        },
-        
-        {id: 15,
             title: 'Software Testing',
             classes : 'fa-brands fa-sistrix skill',
         },
         
-        {id: 16,
+        {id: 14,
             title: 'Debugging',
             classes : 'fa-solid fa-virus-slash skill',
         },
         
+        {id: 15,
+            title: 'Problem Solving',
+            classes : 'fa-solid fa-lightbulb skill',
+        },
+        
+        {id: 16,
+            title: 'GenAI',
+            classes : 'fa-solid fa-robot skill',
+        },      
+        
         {id: 17,
-            title: 'Project Management',
-            classes : 'fa-solid fa-list skill',
+            title: 'Google Workspace',
+            classes : 'fa-brands fa-google skill',
+        },
+        
+        {id: 18,
+            title: 'Communication',
+            classes : 'fa-solid fa-phone skill',
         },
         
         
