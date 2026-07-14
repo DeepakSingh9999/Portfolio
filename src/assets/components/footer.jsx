a03c837fcc1ba080696220b5efda5d274a68337b
Deepak Singh <explorethe.unknowing536@passinbox.com>
2026-07-14 15:16:56 +0530

    Update footer.jsx

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer(){
     return(
         <div className="footer-container">
-            <strong><em>Turning ideas into reality.</em></strong>
+            <strong><em>The knower of truth is never distressed in this world,</em></strong>
+            <strong><em>for the whole round world is full of himself alone.</em></strong>
         </div>
     )
 }
