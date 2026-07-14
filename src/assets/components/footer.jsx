ab487af21aeb90d0376965e593ece5476f004ba3
Deepak Singh <explorethe.unknowing536@passinbox.com>
2026-07-14 15:18:59 +0530

    Update footer.jsx

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -1,8 +1,7 @@
 export default function Footer(){
     return(
         <div className="footer-container">
-            <strong><em>The knower of truth is never distressed in this world,</em></strong>
-            <strong><em> for the whole round world is full of himself alone.</em></strong>
+            <strong><em>The knower of truth is never distressed in this world.</em></strong>
         </div>
     )
 }
