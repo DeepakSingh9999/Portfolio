51cd842a4e899d646eda430a9e1f670e76a9e3aa
Deepak Singh <explorethe.unknowing536@passinbox.com>
2026-08-18 13:58:35 +0530

    Update projects.jsx

--- src/assets/components/projects.jsx
+++ src/assets/components/projects.jsx
@@ -23,7 +23,7 @@ export default function Projects(){
             image: `${projectLink2}`,
             description: `Wild Horizons is a minimalist yet powerful REST-style API that lets you discover some of the world's most unusual, breathtaking, and mysterious destinations. Built using pure Node.js to showcase strong fundamentals and clean architecture.`,
             techStack: 'JavaScript, Node.js',
-            liveLink: 'https://github.com/DeepakSingh9999/WildHorizons',
+            liveLink: 'https://wild-horizons-eight.vercel.app/api',
             viewCode: 'https://github.com/DeepakSingh9999/WildHorizons'
         },
         
